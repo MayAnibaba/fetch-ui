a159653f73c6f86133d8b80e9f47ec2b4e932c40
May <97801810+MayAnibaba@users.noreply.github.com>
2023-01-25 22:56:22 +0100

    more middleware data

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -6,6 +6,31 @@ class User extends BaseController
 {
     public function index(){
 
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://localhost:3000/users',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 60,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+        
+        $response = curl_exec($curl);
+        curl_close($curl);
+		
+        //print_r($response);
+
+        $responseObject = json_decode($response);
+        print_r($responseObject);
+
+
+		if($responseObject->code =="00"){
+            $data['users'] = $responseObject->data;
+        } else {
+            $session->setFlashdata('msg', $responseObject->message);
+        }
+
         $data['user_email'] = session()->get('email');
         $this->template('user/list', $data); 
     }
