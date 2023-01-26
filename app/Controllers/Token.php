7a48cf8f3e9e581cedc5a05c19dfbed3dec9f832
May <97801810+MayAnibaba@users.noreply.github.com>
2023-01-26 22:59:59 +0100

    paystack transaction initialisation

--- app/Controllers/Token.php
+++ app/Controllers/Token.php
@@ -6,20 +6,56 @@ class Token extends BaseController
 {
     public function create(){
 
+        print_r($_GET);
+        if(isset($_GET['id'])){
+
+            $loanRef = htmlspecialchars($_GET['id']);
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'http://localhost:3000/loans/byLoanRef',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_TIMEOUT => 60,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS =>'{
+                    "loanRef":"'.$loanRef.'"}',
+                CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
+            ));
+            
+            $response = curl_exec($curl);
+            curl_close($curl);
+            
+            //print_r($response);
+
+            $responseObject = json_decode($response);
+            //print_r($responseObject);
+
+
+            if($responseObject->code =="00"){
+                //paystack transaction initialize
+                $secretKey = 'sk_test_45213c0b37221dc3a914a01a3ebace00f47f82c2';
+                $Transaction = new \Matscode\Paystack\Transaction( $secretKey );
+                $data = 
+                [
+                    'email'  => $responseObject->data->email,
+                    'amount' => 5000 // amount is treated in kobo using this method
+                ];
+                $response = $Transaction->initialize($data);
         
+                print_r($response);
+                return view('token/create', );
 
-        $secretKey = 'sk_test_45213c0b37221dc3a914a01a3ebace00f47f82c2';
-        // creating the transaction object
-        $Transaction = new \Matscode\Paystack\Transaction( $secretKey );
-        $data = 
-        [
-            'email'  => 'customer@email.com',
-            'amount' => 500000 // amount is treated in kobo using this method
-        ];
-        $response = $Transaction->initialize($data);
+            } else {
+                echo "Error wrong ref";
+                //$session->setFlashdata('msg', $responseObject->message);
+            }
 
-        print_r($response);
-        return view('token/create', );
+        } else {
+            echo "Error no ref";
+        }
+       
     }
 
     public function call_back(){
