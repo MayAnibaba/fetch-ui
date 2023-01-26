ade0bf9b2f08238498500ea22aec6cff194d7e7d
May <97801810+MayAnibaba@users.noreply.github.com>
2023-01-26 15:25:09 +0100

    users complete
    with bad UI

--- app/Views/user/list.php
+++ app/Views/user/list.php
@@ -9,13 +9,22 @@
             <h6 class="m-0 font-weight-bold text-primary">Users</h6>
         </div>
         <div class="card-body">
+        <div class="col-sm-12 col-md-6" style="padding:">
+                <a href="#" class="btn btn-primary btn-icon-split">
+                    <span class="icon text-white-50">
+                        <i class="fas fa-flag"></i>
+                    </span>
+                    <span class="text">Add user</span>
+                </a>
+        </div>
+
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
                             <th>Email</th>
                             <th>Status</th>
-                            <th>Lastlogin</th>
+                            <th>Last login</th>
                             <th>Created </th>
                             <th>Last updated</th>
                             <th>Actions</th>
@@ -25,7 +34,7 @@
                     <tr>
                             <th>Email</th>
                             <th>Status</th>
-                            <th>Lastlogin</th>
+                            <th>Last login</th>
                             <th>Created </th>
                             <th>Last updated</th>
                             <th>Actions</th>
@@ -41,7 +50,13 @@
                             <td><?= $users[$i]->lastLogin?></td>
                             <td><?= $users[$i]->createdAt?></td>
                             <td><?= $users[$i]->updatedAt?></td>
-                            <td></td>
+                            <td><a href="#" title="edit" class="btn btn-info btn-circle btn-sm">
+                                        <i class="fas fa-info-circle"></i>
+                                    </a>
+                                    <a href="#" title="delete" class="btn btn-danger btn-circle btn-sm">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                                </td>
                         </tr>
                     <?php  } ?>
                        
