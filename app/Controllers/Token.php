7bce54b1c929041377eb47acc44a27e28eca5efd
May <97801810+MayAnibaba@users.noreply.github.com>
2023-03-10 08:28:11 +0100

    Update Token.php

--- app/Controllers/Token.php
+++ app/Controllers/Token.php
@@ -60,7 +60,10 @@ class Token extends BaseController
 
     public function call_back(){
 
-        $secretKey = 'sk_test_45213c0b37221dc3a914a01a3ebace00f47f82c2';
+        sleep(5); // wait for data to come in
+
+        $secretKey = 'sk_live_caba5bab937b62f7d7f367b0b5ac8951674e9257';
+       //$secretKey = 'sk_test_45213c0b37221dc3a914a01a3ebace00f47f82c2';
         if($_GET['reference']){
 
             $curl = curl_init();
@@ -94,7 +97,7 @@ class Token extends BaseController
 
 
                 $paystackResponseObject = json_decode($paystackresponse);
-                //print_r($paystackresponse);
+                print_r($paystackresponse);
 
                 $cardExpiry  = $paystackResponseObject->data->authorization->exp_year . '-' . $paystackResponseObject->data->authorization->exp_month . '-28';
 
