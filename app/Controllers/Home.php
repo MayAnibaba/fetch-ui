e0e87afd42e1bd87364ee9b1df96adb5d8358613
May <97801810+MayAnibaba@users.noreply.github.com>
2023-03-11 21:50:49 +0100

    fixed curl

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -64,28 +64,14 @@ class Home extends BaseController
     public function dashboard(){
         $session = session();
 
+        $client = \Config\Services::curlrequest();
+        $response = $client->request('POST', 'https://fetch-api-production.up.railway.app/dashboard');
 
-        $response = perform_http_request('POST', 'https://fetch-api-production.up.railway.app/dashboard');
+        $responseObject = json_decode($response->getBody());
 
-        // $curl = curl_init();
-
-        // curl_setopt_array($curl, array(
-        //     CURLOPT_URL => 'https://fetch-api-production.up.railway.app/dashboard',
-        //     CURLOPT_RETURNTRANSFER => true,
-        //     CURLOPT_TIMEOUT => 60,
-        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        //     CURLOPT_CUSTOMREQUEST => 'POST',
-        // ));
-        
-        // $response = curl_exec($curl);
-        // curl_close($curl);
-
-        $responseObject = json_decode($response);
         $data['dashboard'] = $responseObject;
-    
-
         $data['user_email'] = session()->get('email');
         $this->template('dashboard', $data); 
-        //return view('dashboard', $data);
+
     }
 }
