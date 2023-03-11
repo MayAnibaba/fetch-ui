ebac556ff5f534039bf21722567a0ff6119a9f1f
May <97801810+MayAnibaba@users.noreply.github.com>
2023-03-11 21:37:30 +0100

    adding curl

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -39,7 +39,7 @@ abstract class BaseController extends Controller
      *
      * @var array
      */
-    protected $helpers = [];
+    protected $helpers = ['Curl'];
 
     /**
      * Be sure to declare properties for any property fetch you initialized.
